d7f2d730883f00cf737ff4d1b22e3686eddc6560
Maciej Skrzypinski <maskrz@gmail.com>
2015-03-28 19:31:16 +0100

    researches

---
+++ src/algorithm/choice/QuestionChoiceStrategyType.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package algorithm;
+
+/**
+ *
+ * @author Skrzypek
+ */
+public enum SelectionHelperType {
+    NONE,
+    ADD,
+    MULTIPLE,
+    ONLY_ENTROPY,
+    RANKING,
+    PERCENTAGE_RANKING,
+    BOLTZMANN_ENTROPY;
+}
